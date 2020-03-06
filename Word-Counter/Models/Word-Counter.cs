385195afc2b3e3f40111448aeca7d22d64572b1b
Geoff Goetz <geoff.goetz503@gmail.com>
2020-03-06 11:02:49 -0800

    Add code to pass first test. Test Passes.

--- Word-Counter/Models/Word-Counter.cs
+++ Word-Counter/Models/Word-Counter.cs
@@ -4,10 +4,14 @@ namespace WordCounter.Models
 {
   public class WordCount
   {
-    // public static void Temp()
-    // {
-      
-    // }
+    public string Word { get; set; }
+    public string Sentence { get; set; }
+
+    public void Count(string inputWord, string inputSentence)
+    {
+      inputWord = Word;
+      inputSentence = Sentence;
+    }
   }
 
 }
