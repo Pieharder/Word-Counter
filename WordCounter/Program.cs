4f6c38d9657ae9c6e4bdf74aeeed54f2748f9c63
Geoff Goetz <geoff.goetz503@gmail.com>
2020-03-16 20:13:56 -0700

    Added front end for user input

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,7 +7,13 @@ namespace WordCounter
   {
     public static void Main()
     {
-     
+      Console.WriteLine("Input a sentence:");
+      string userInputSentence = Console.ReadLine();
+      Console.WriteLine("Input a word from that sentence to count:");
+      string userInputWord = Console.ReadLine();
+      WordCount newWordCount = new WordCount(userInputWord, userInputSentence);
+      int count = newWordCount.WordCounting();
+      Console.WriteLine("Your word appears " + count + " times in this sentence.");
     }
   }
 
